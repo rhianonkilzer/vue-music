de71a45d08c6c06765e06fd80b77fc9d1da7f7f8
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-31 15:00:37 -0600

    push

---
+++ client/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Home">
+    
+  </div>
+</template>
+
+<script>
+import MySong from "@/components/MySong";
+import Song from "@/components/Song";
+
+export default {
+  name: 'Home',
+  data() {
+    return{
+      MySong:{},
+      Song: {},
+      Playlist: {}
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Song.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+</template>
+<script>
+    export default {
+        name: "Song",
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+
+        }
+    }
+</script>
+<style scoped>
+</style>
